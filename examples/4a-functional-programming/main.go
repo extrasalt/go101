6ed72cda9a294c13467fdd188958ef0de10e3ede
Mohanarangan Muthukumar <mohan@extrasalt.org>
2021-03-24 21:57:55 +0530

    Add higher order function example

--- examples/4a-functional-programming/main.go
+++ examples/4a-functional-programming/main.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
+func fmap(slice []int, f func(int) int) []int {
+	result := []int{}
+	for _, v := range slice {
+		result = append(result, f(v))
+	}
+
+	return result
+}
+
+func square(n int) int {
+	return n * n
+}
+
 func main() {
+	o := fmap([]int{3, 5, 9}, square)
+
+	fmt.Println(o)
 
 }
