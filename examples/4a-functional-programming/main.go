ab1b843019889d894fb7091706d6e5f7f97f6337
Mohanarangan Muthukumar <mohan@extrasalt.org>
2021-03-24 22:03:06 +0530

    Add function as return values example

--- examples/4a-functional-programming/main.go
+++ examples/4a-functional-programming/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func fmap(slice []int, f func(int) int) []int {
 	result := []int{}
@@ -15,9 +18,18 @@ func square(n int) int {
 	return n * n
 }
 
+func power(p int) func(int) int {
+	return func(n int) int {
+		return int(math.Pow(float64(n), float64(p)))
+	}
+}
+
 func main() {
 	o := fmap([]int{3, 5, 9}, square)
+	fmt.Println(o)
 
+	cube := power(3)
+	o = fmap([]int{3, 5, 9}, cube)
 	fmt.Println(o)
 
 }
